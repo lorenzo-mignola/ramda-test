56ea82154896b094b65fdc21a46df22d7478ebdd
lorenzo-mignola <lo.mignola@gmail.com>
2022-04-18 17:58:08 +0200

    Add test full code

--- __test__/codiceFiscale.test.js
+++ __test__/codiceFiscale.test.js
@@ -1,9 +1,9 @@
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import { slice } from 'ramda';
 import { codiceFiscale } from '../lib/codiceFiscale/codiceFiscale.js';
 
-const testSurCodeName = slice(0, 3);
-const testCodeName = slice(3, 6);
+const testSurexpectedName = slice(0, 3);
+const testexpectedName = slice(3, 6);
 const testNumberDate = slice(6, 8);
 const testLetterMonth = slice(8, 9);
 const testNumberDay = slice(9, 11);
@@ -14,7 +14,7 @@ describe('codiceFiscale surname', () => {
 
     const codice = codiceFiscale(person);
 
-    expect(testSurCodeName(codice)).to.equal('NWM');
+    expect(testSurexpectedName(codice)).to.equal('NWM');
   });
 
   it('should return 3 consonant with vowels', () => {
@@ -22,7 +22,7 @@ describe('codiceFiscale surname', () => {
 
     const codice = codiceFiscale(person);
 
-    expect(testSurCodeName(codice)).to.equal('FXO');
+    expect(testSurexpectedName(codice)).to.equal('FXO');
   });
 
   it('should add 1 X', () => {
@@ -30,7 +30,7 @@ describe('codiceFiscale surname', () => {
 
     const codice = codiceFiscale(person);
 
-    expect(testSurCodeName(codice)).to.equal('YUX');
+    expect(testSurexpectedName(codice)).to.equal('YUX');
   });
 });
 
@@ -40,7 +40,7 @@ describe('codiceFiscale name', () => {
 
     const codice = codiceFiscale(person);
 
-    expect(testCodeName(codice)).to.equal('MTT');
+    expect(testexpectedName(codice)).to.equal('MTT');
   });
 
   it('should return 3 consonant from a name with more than 3 consonant', () => {
@@ -48,7 +48,7 @@ describe('codiceFiscale name', () => {
 
     const codice = codiceFiscale(person);
 
-    expect(testCodeName(codice)).to.equal('SNT');
+    expect(testexpectedName(codice)).to.equal('SNT');
   });
 
   it('should return 3 consonant with a vowel', () => {
@@ -56,7 +56,7 @@ describe('codiceFiscale name', () => {
 
     const codice = codiceFiscale(person);
 
-    expect(testCodeName(codice)).to.equal('BBO');
+    expect(testexpectedName(codice)).to.equal('BBO');
   });
 
   it('should add 1 X', () => {
@@ -64,7 +64,7 @@ describe('codiceFiscale name', () => {
 
     const codice = codiceFiscale(person);
 
-    expect(testCodeName(codice)).to.equal('LAX');
+    expect(testexpectedName(codice)).to.equal('LAX');
   });
 });
 
@@ -158,5 +158,80 @@ describe('codiceFiscale date', () => {
     const codice = codiceFiscale(person);
 
     expect(testNumberDay(codice)).to.equal('60');
+  });
+});
+
+describe('test full expected', () => {
+  const people = [
+    {
+      person: {
+        name: 'Matt',
+        surname: 'Edabit',
+        gender: 'M',
+        dob: '1/1/1900'
+      },
+      expected: 'DBTMTT00A01'
+    },
+    {
+      person: {
+        name: 'Helen',
+        surname: 'Yu',
+        gender: 'F',
+        dob: '1/12/1950'
+      },
+      expected: 'YUXHLN50T41'
+    },
+    {
+      person: {
+        name: 'Mickey',
+        surname: 'Mouse',
+        gender: 'M',
+        dob: '16/1/1928'
+      },
+      expected: 'MSOMKY28A16'
+    },
+    {
+      person: {
+        name: 'Brendan',
+        surname: 'Eich',
+        gender: 'M',
+        dob: '1/12/1961'
+      },
+      expected: 'CHEBND61T01'
+    },
+    {
+      person: { name: 'Helen', surname: 'Yu', gender: 'F', dob: '1/12/1950' },
+      expected: 'YUXHLN50T41'
+    },
+    {
+      person: {
+        name: 'Mickey',
+        surname: 'Mouse',
+        gender: 'M',
+        dob: '16/1/1928'
+      },
+      expected: 'MSOMKY28A16'
+    },
+    {
+      person: { name: 'Al', surname: 'Capone', gender: 'M', dob: '17/1/1899' },
+      expected: 'CPNLAX99A17'
+    },
+    {
+      person: {
+        name: 'Marie',
+        surname: 'Curie',
+        gender: 'F',
+        dob: '7/11/1867'
+      },
+      expected: 'CRUMRA67S47'
+    }
+  ];
+
+  people.forEach(({ person, expected }) => {
+    it(`correctly generate expected for ${person.name} ${person.surname}`, () => {
+      const codice = codiceFiscale(person);
+      console.log('clg -> it -> codice', codice);
+      assert.strictEqual(codice, expected);
+    });
   });
 });

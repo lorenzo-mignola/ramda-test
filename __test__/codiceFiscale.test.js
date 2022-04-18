778ac74b31a37ce68c0a2f3623e4f24955e77b6b
lorenzo-mignola <lo.mignola@gmail.com>
2022-04-18 16:04:22 +0200

    Rename functions

--- __test__/codiceFiscale.test.js
+++ __test__/codiceFiscale.test.js
@@ -1,28 +1,31 @@
 import { expect } from 'chai';
+import { slice } from 'ramda';
 import { codiceFiscale } from '../lib/codiceFiscale/codiceFiscale.js';
+
+const testSurCodeName = slice(0, 3);
 
 describe('codiceFiscale', () => {
   it('should return 3 consonant', () => {
-    const person = { name: 'Newman', surname: null, gender: null, dob: null };
+    const person = { name: null, surname: 'Newman', gender: null, dob: null };
 
     const codice = codiceFiscale(person);
 
-    expect(codice).to.equal('NWM');
+    expect(testSurCodeName(codice)).to.equal('NWM');
   });
 
   it('should return 3 consonant with vowels', () => {
-    const person = { name: 'Fox', surname: null, gender: null, dob: null };
+    const person = { name: null, surname: 'Fox', gender: null, dob: null };
 
     const codice = codiceFiscale(person);
 
-    expect(codice).to.equal('FXO');
+    expect(testSurCodeName(codice)).to.equal('FXO');
   });
 
   it('should add 1 X', () => {
-    const person = { name: 'Yu', surname: null, gender: null, dob: null };
+    const person = { name: null, surname: 'Yu', gender: null, dob: null };
 
     const codice = codiceFiscale(person);
 
-    expect(codice).to.equal('YUX');
+    expect(testSurCodeName(codice)).to.equal('YUX');
   });
 });

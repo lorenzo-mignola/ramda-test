e9b8f4acc12fca682a83b9e8d467ce6ed7077dea
lorenzo-mignola <lo.mignola@gmail.com>
2022-04-18 15:10:44 +0200

    Creato codice fiscale

---
+++ __test__/codiceFiscale.test.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import { codiceFiscale } from '../lib/codiceFiscale.js';
+
+describe('codiceFiscale', () => {
+  it('should return 3 consonant', () => {
+    const person = { name: 'Newman', surname: null, gender: null, dob: null };
+
+    const codice = codiceFiscale(person);
+
+    expect(codice).to.equal('NWM');
+  });
+  it('should return 3 consonant with vowels', () => {
+    const person = { name: 'Fox', surname: null, gender: null, dob: null };
+
+    const codice = codiceFiscale(person);
+
+    expect(codice).to.equal('FXO');
+  });
+});
